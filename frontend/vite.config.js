45d8e31c437914eb2edc190c9d2095fc6855b58c
Ilia Savchenko <ilia_prog@mail.ru>
2026-09-09 20:17:49 +0300

    frontend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/brand-click-tracker/' : '/',
   plugins: [react()],
 })
